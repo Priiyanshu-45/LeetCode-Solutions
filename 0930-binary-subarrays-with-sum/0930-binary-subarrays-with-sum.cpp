74c83dae7ae2be9c99359a420959eafe6b239735
Priiyanshu-45 <priyanshu280407@gmail.com>
2026-06-27 19:50:24 +0530

    Time: 0 ms (100%), Space: 32.5 MB (98.57%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
+    // int n = nums.size();
+    //         int cnt = 0;
+    //         for(int i = 0; i<n; i++){
+    //             int sum = 0;
+    //             for(int j = i; j<n; j++){
+    //                 sum += nums[j];
+    //                 if(sum == goal) cnt++;
+    //                 else if (sum > goal) break;
+    //             }
+    //         }
+    //         return cnt;
+    
+    int atMost(vector<int>& nums, int goal){
+        if(goal < 0) return 0;
         int n = nums.size();
         int cnt = 0;
-        for(int i = 0; i<n; i++){
-            int sum = 0;
-            for(int j = i; j<n; j++){
-                sum += nums[j];
-                if(sum == goal) cnt++;
-                else if (sum > goal) break;
+        int i = 0; 
+        int sum = 0;
+        for(int j = 0; j<n; j++){
+            sum += nums[j];
+            while(sum > goal){
+                sum -= nums[i];
+                i++;
             }
+            cnt += j-i+1;
         }
         return cnt;
+    }
+
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return atMost(nums, goal) - atMost(nums, goal-1);
     }
 };
