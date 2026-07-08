ef87434f6b601d9e0dd02939372b36dc4b9f4110
Priiyanshu-45 <priyanshu280407@gmail.com>
2026-07-08 17:26:25 +0530

    Time: 6 ms (39.76%), Space: 28.4 MB (63.61%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
+    // int n = nums.size();
+    //         nums.reserve(n*2);
+    //         nums.insert(nums.end(), nums.begin(), nums.end());
+    //         vector<int> ans;
+    //         for(int i = 0; i<n; i++){
+    //             bool fnd = false;
+    //             for(int j = i; j<2*n; j++){
+    //                 if(nums[i] < nums[j]){
+    //                     ans.push_back(nums[j]);
+    //                     fnd = true;
+    //                     break;
+    //                 }
+    //             }
+    //             if(!fnd) ans.push_back(-1);
+    //         }
+    //         return ans;
+
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n = nums.size();
-        nums.reserve(n*2);
-        nums.insert(nums.end(), nums.begin(), nums.end());
-        vector<int> ans;
-        for(int i = 0; i<n; i++){
-            bool fnd = false;
-            for(int j = i; j<2*n; j++){
-                if(nums[i] < nums[j]){
-                    ans.push_back(nums[j]);
-                    fnd = true;
-                    break;
-                }
+        vector<int> ans(n);
+        stack<int> st;
+        for(int i = 2*n-1; i>=0; i--){
+            int ind = i%n;
+            while(!st.empty() && nums[ind] >= st.top()){
+                st.pop();
             }
-            if(!fnd) ans.push_back(-1);
+            if(st.empty()){
+                ans[ind] = -1;
+            }
+            else {
+                ans[ind] = st.top();
+            }
+                st.push(nums[ind]);
         }
+        
         return ans;
     }
 };
